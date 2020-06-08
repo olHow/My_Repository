15119c6959ca4de99c5249c64b29f7f1fbb538a8
Olivier Howaizi <olivierhowaizi@air-de-olivier.home>
2020-06-08 15:14:26 +0200

    Req & Body validation // register user

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-// @route            GET API/User
-// @description      Test route
+const { check, validationResult } = require('express-validator');
+
+// @route            POST API/User
+// @description      Register user
 // @access           Public
-router.get('/', (req, res) => res.send('User route'));
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Please Include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with minimum 6 characters'
+    ).isLength({ min: 6 }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send('User route');
+  }
+);
 
 module.exports = router;
