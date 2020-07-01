102f7a6d61f05f06e70d2f08c9b03b4d7de605b6
Olivier Howaizi <olivierhowaizi@MacBook-Air-de-Olivier.local>
2020-07-01 09:33:33 +0200

    36... 37. Creating a Redux Store

---
+++ client/src/store.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers'; //il faut ajouter un fichier reducers dans lequel on met un nouvel index.js
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
