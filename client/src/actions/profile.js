2b93f949b86b2ad72ed713e78839a01ffb407e94
Olivier Howaizi <olivierhowaizi@MacBook-Air-de-Olivier.local>
2020-09-16 16:29:36 +0200

    Finish Profile Actions & Reducer

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -6,6 +6,8 @@ import {
   UPDATE_PROFILE,
   CLEAR_PROFILE,
   ACCOUNT_DELETED,
+  GET_PROFILES,
+  GET_REPOS,
 } from './types';
 
 //Get my current profile
@@ -14,6 +16,49 @@ export const getCurrentProfile = () => async (dispatch) => {
   try {
     const res = await axios.get('api/profile/me');
     dispatch({ type: GET_PROFILE, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Get all profiles
+
+export const getProfiles = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
+  try {
+    const res = await axios.get('api/profile');
+    dispatch({ type: GET_PROFILES, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Get profile by ID
+
+export const getProfileByID = (userID) => async (dispatch) => {
+  try {
+    const res = await axios.get(`api/profile/user/${userID}`);
+    dispatch({ type: GET_PROFILE, payload: res.data });
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+//Get GitHUB Repos
+
+export const getGitHubRepos = (username) => async (dispatch) => {
+  try {
+    const res = await axios.get(`API/profile/github/${username}`);
+    dispatch({ type: GET_REPOS, payload: res.data });
   } catch (err) {
     dispatch({
       type: PROFILE_ERROR,
